48a8f12fab05c824e89fc8ab62057486b97818b0
drose <david.rose@swftechnology.com>
2021-05-25 11:57:06 -0400

    Shifting graph developments

--- src/Graphs/EEG_Graph.tsx
+++ src/Graphs/EEG_Graph.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/order */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   Label,
@@ -9,6 +9,7 @@ import {
   XAxis,
   YAxis,
 } from 'recharts';
+import * as d3 from "d3";
 import getChannelData from '../Actions/GetChannelData';
 
 export default function EEGGraph() {
@@ -66,10 +67,13 @@ export default function EEGGraph() {
     };
   }, [client, client && client.connected]); */
 
+  interface dataType { time: number, value: number }
   const dispatch = useDispatch()
   const currentData = useSelector((state : any) => state.allChannelsReducer.currentData)
+  const [shiftingData, setShiftingData] = useState<dataType[]>([])
 
   useEffect(() => {
+    seedData();
     client.onopen = () => {
       console.log('WebSocket Client Connected');
       client.send('MESSAGE');
@@ -87,35 +91,127 @@ export default function EEGGraph() {
     };
   }, []);  
 
+  useEffect(()=>{
+    let val = currentData[currentData.length - 1].channel1;
+    console.log(val);
+    updateData(val);
+  }, [currentData]);
+
+  useEffect(()=>{
+    drawChart()
+  }, [shiftingData])
+
+  //initialize the data being displayed in line chart
+  function seedData(){
+    let tempShiftingData: any = []
+    for(let i=1;i<=100;i++){
+      let flatData = {
+        time: i,
+        value: 0
+      }
+      //push object to end of array
+      tempShiftingData.push(flatData)
+    }
+    setShiftingData(tempShiftingData)
+  }
+  
+  function updateData(val: number){
+    let tempShiftingData = shiftingData
+    //(shift removes first element, shifting might make implementation easier)
+    tempShiftingData.pop()
+    tempShiftingData.map((obj: { time: number, value: number }) => {
+      obj.time = obj.time+1
+    })
+    let newData = {
+      time: 1,
+      value: val
+    }
+    tempShiftingData.unshift(newData)
+    setShiftingData(tempShiftingData)
+  }
+
+  function drawChart(){
+    d3.select('#chart')
+      .select('svg')
+      .remove();
+
+    const width = 300
+    const height = 100
+    const margin = { top: 50, right: 50, bottom: 50, left: 50 }
+
+    const yMinVal = d3.min(shiftingData, d => d.value) as number
+    const yMaxValue = d3.max(shiftingData, d => d.value) as number
+    const xMinValue = 1   //d3.min(shiftingData, d => d.time)
+    const xMaxValue = 100 //d3.max(shiftingData, d => d.time)
+    
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.bottom})`)
+    //const tooltip = d3.select('#container').append('div').attr('class', 'tooltip');
+
+    const xScale = d3.scaleLinear()
+      .domain([xMinValue, xMaxValue]).range([0, width])
+    const yScale = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0, yMaxValue])
+    const line = d3.line<dataType>()
+      .x(d => xScale(d.time))
+      .y(d => yScale(d.value))
+      //curve(d3.curveMonotoneX);
+    
+      svg.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(xScale).tickSize(15));
+      svg.append('g')
+        .attr('class', 'y-axis')
+        .call(d3.axisLeft(yScale));
+      svg.append('path')
+        .datum(shiftingData)
+        .attr('fill', 'none')
+        .attr('stroke', '#f6c3d0')
+        .attr('stroke-width', 4)
+        .attr('class', 'line') 
+        .attr('d', line);
+  }
+
   return (
-    <LineChart
-      width={800}
-      height={400}
-      data={currentData}
-      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-    >
-      <XAxis
-        dataKey="time"
-        // tickFormatter={(timeStr) => moment(timeStr).format('HH:mm')}
-      />
-      <YAxis>
-        <Label
-          value="micro Volts"
-          position="left"
-          angle={-90}
-          style={{ textAnchor: 'middle' }}
+    <div>
+      <LineChart
+        width={800}
+        height={400}
+        data={currentData}
+        margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+      >
+        <XAxis
+          dataKey="time"
+          // tickFormatter={(timeStr) => moment(timeStr).format('HH:mm')}
         />
-      </YAxis>
-      {/* <Tooltip />
-       <CartesianGrid stroke="#f5f5f5" /> */}
-      <Line
-        activeDot
-        type="monotone"
-        dataKey="channel1"
-        stroke="#ff7300"
-        yAxisId={0}
-      />
-      {/* <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} /> */}
-    </LineChart>
+        <YAxis>
+          <Label
+            value="micro Volts"
+            position="left"
+            angle={-90}
+            style={{ textAnchor: 'middle' }}
+          />
+        </YAxis>
+        {/* <Tooltip />
+        <CartesianGrid stroke="#f5f5f5" /> */}
+        <Line
+          activeDot
+          type="monotone"
+          dataKey="channel1"
+          stroke="#ff7300"
+          yAxisId={0}
+        />
+        {/* <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} /> */}
+      </LineChart>
+      <hr />
+      <div id="chart"></div>
+    </div>
   );
 }
